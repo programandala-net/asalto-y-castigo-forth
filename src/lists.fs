42cc7c737774416d2cbaf0d7643b075db2008824
Marcos Cruz <github@programandala.net>
2017-08-14 18:58:43 +0200

    Translate the lists module; update its source style

--- src/lists.fs
+++ src/lists.fs
@@ -5,11 +5,11 @@
 
 \ Author: Marcos Cruz (programandala.net), 2011..2016
 
-\ Last modified 201607212220
-
-\ Note: The comments of the code are in Spanish.
+\ Last modified 201708141855
+\ See change log at the end of the file
 
 \ ==============================================================
+\ Requirements
 
 get-current forth-wordlist set-current
 
@@ -32,86 +32,92 @@ set-current
 require out-str.fs
 
 \ ==============================================================
+\ Main
 
 variable #listed
-  \ Contador de elementos listados, usado en varias acciones.
+  \ Count of elements already listed.
 
 variable #elements
-  \ Total de los elementos de una lista.
+  \ Total count of list elements.
 
-: list-separator$  ( u1 u2 -- ca len )
+: list-separator$ ( u1 u2 -- ca len )
   ?dup if
-    1+ = if  s"  y "  else  s" , "  then
-  else  0  then  ;
-  \ Devuelve el separador adecuado a un elemento de una lista.
-  \ u1 = Elementos que tiene la lista
-  \ u2 = Elementos listados hasta el momento
-  \ ca len = Cadena devuelta, que podrá ser « y » o «, » o «» (vacía)
+    1+ = if s"  y " else s" , " then
+  else 0 then ;
+  \ Return the proper separator _ca len_, "y" ("and" in Spanish), ","
+  \ or nothing, to the dynamic string `out-str`, being _u1_ the total
+  \ number of items in the list and _u2_ the number of items already
+  \ listed.
 
-: (list-separator)  ( u1 u2 -- )
-  1+ = if  s" y" out-str str-append-txt  else  s" ," out-str str-append-string  then  ;
-  \ Añade a la cadena dinámica `out-str` el separador adecuado («y»
-  \ o «,») para un elemento de una lista, siendo _u1_ el número de
-  \ elementos que tiene la lista y _u2_ el número de elementos ya listados.
+: (list-separator) ( u1 u2 -- )
+  1+ = if   s" y" out-str str-append-txt
+       else s" ," out-str str-append-string then ;
+  \ Add the proper separator, "y" ("and" in Spanish) or ",", to the
+  \ dynamic string `out-str`, being _u1_ the total number of items in
+  \ the list and _u2_ the number of items already listed.
 
-: list-separator  ( u1 u2 -- )
-  ?dup if  (list-separator)  else  drop  then  ;
-  \ Añade a la cadena dinámica `out-str` el separador adecuado (o
-  \ ninguno) para un elemento de una lista, siendo _u1_ el número de
-  \ elementos que tiene la lista y _u2_ el número de elementos ya
-  \ listados.
+: list-separator ( u1 u2 -- )
+  ?dup if (list-separator) else drop then ;
+  \ Add the proper separator (or nothing) to the dynamic string
+  \ `out-str`, being _u1_ the total number of items in the list and
+  \ _u2_ the number of items already listed.
 
-: /list++  ( u a1 a2 -- u | u+1 )
+: /list++ ( u entity1 entity2 -- u | u+1 )
   dup must-be-listed?
-  if  location = abs +  else  2drop  then  ;
-  \ Incrementa el contador _u_ si un ente _a1_ es la localización de
-  \ otro ente _a2_ y puede ser listado.
+  if location = abs + else 2drop then ;
+  \ If _entity1_ can be listed and it's the location of _entity2_,
+  \ increment counter _u_.
 
-: /list  ( a -- u )
-  0  \ Contador
-  #entities 0 do  over i #>entity /list++  loop  nip  ;
-  \ Cuenta el número de entes _u_ cuya localización es el ente _a_ y
-  \ pueden ser listados.
+: /list ( entity -- u )
+  0 \ Contador
+  #entities 0 do over i #>entity /list++ loop nip ;
+  \ Return number _u_ of entities whose location is _entity_ and
+  \ can be listed.
 
-: (worn)$  ( a -- ca1 len1 )
-  s" (puest" rot noun-ending s" )" s+ s+  ;
-  \ Devuelve «(puesto/a/s)», según el género y número del ente indicado.
+: (worn)$ ( entity -- ca len )
+  s" (puest" rot noun-ending s" )" s+ s+ ;
+  \ Return string _ca len_ containing "(puesto/a/s)" ("worn" in
+  \ Spanish), depending on the gender and number of _entity_.
 
-: (worn)&  ( ca1 len1 a -- ca1 len1 | ca2 len2 )
-  dup  is-worn? if  (worn)$ txt+  else  drop  then  ;
-  \ Añade a una cadena _ca1 len1_, si es necesario, el indicador de
-  \ que el ente _a_ es una prenda puesta, devolviendo una nueva cadena
-  \ _ca2 len2_ con el resultado.
+: (worn)& ( ca1 len1 a -- ca1 len1 | ca2 len2 )
+  dup is-worn? if (worn)$ txt+ else drop then ;
+  \ If needed, add a note to string _ca1 len1_, to indicate
+  \ _entity_ is a worn cloth, returning the result string _ca2 len2_.
 
-: (content-list)  ( a -- )
-  #elements @ #listed @  list-separator
-  dup full-name-as-direct-complement rot (worn)& out-str str-append-txt  #listed ++  ;
-  \ Añade a la lista en la cadena dinámica `out-str` el separador y
-  \ el nombre de un ente.
+: (content-list) ( entity -- )
+  #elements @ #listed @ list-separator
+  dup full-name-as-direct-complement
+  rot (worn)& out-str str-append-txt #listed ++ ;
+  \ Add name of _entity_ (with a proper separator), to the list
+  \ hold in dynamic srting `out-str`.
 
-: about-to-list  ( a -- n )  #listed off  /list dup #elements !  ;
-  \ Prepara el inicio de una lista, siendo _a_ el ente que es la
-  \ localización de los entes a incluir en la lista; y _n_ el número
-  \ de entes que serán listados.
+: about-to-list ( entity -- n ) #listed off /list dup #elements ! ;
+  \ Prepare a list of entities whose location is _entity_, and
+  \ return the number _n_ of entities that will be listed.
 
-: content-list  ( a -- ca len )
+: content-list ( entity -- ca len )
   out-str str-clear
   dup about-to-list if
     #entities 1 do
       dup i #>entity dup must-be-listed? if
-        is-there? if  i #>entity (content-list)  then
-      else  2drop
-      then
-    loop  s" ." out-str str-append-string
-  then  drop  out-str str-get  ;
-  \ Devuelve una lista _ca len_ de entes cuya localización es el
-  \ ente _a_.
+        is-there? if i #>entity (content-list) then
+      else 2drop then
+    loop s" ." out-str str-append-string
+  then drop out-str str-get ;
+  \ Return a comma-separated list _ca len_ of entities whose location
+  \ is _entity_.
 
-: .present  ( -- )
+: .present ( -- )
   my-location content-list dup
-  if  s" Ves" s" Puedes ver" 2 2choose 2swap txt+ narrate
-  else  2drop  then  ;
-  \ Lista los entes presentes.
+  if   s" Ves" s" Puedes ver" 2 2choose 2swap txt+ narrate
+  else 2drop then ;
+  \ List the present entities.
+
+\ ==============================================================
+\ Change log
+
+\ 2017-08-14: Translate comments from Spanish to English. Update
+\ source style and stack notation.
 
 \ vim:filetype=gforth:fileencoding=utf-8
 
